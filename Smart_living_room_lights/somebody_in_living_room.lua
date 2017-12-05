07fd0cdb456c7a66215c6f90c6ddbca7389f260c
Nick Shores <nick@shoresmedia.com>
2017-12-05 12:36:36 -0800

    added night time support

--- Smart_living_room_lights/somebody_in_living_room.lua
+++ Smart_living_room_lights/somebody_in_living_room.lua
@@ -1,5 +1,7 @@
 LastTripInLivingRoom = os.time()
 HumanInLivingRoom = 1
 --log("Detected movement in living room, turn on living light")
-turnOnLight(LivingRoomLight)
-
+if luup.is_night() == true then
+    turnOnLight(LivingRoomLight)
+    else
+end
