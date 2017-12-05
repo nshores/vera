72d19bdb3a2b53ea51620bf2467c52dfd6477908
Nick Shores <nick@shoresmedia.com>
2017-12-05 12:30:45 -0800

    fixed text

--- Smart_living_room_lights/nobody_living_room.lua
+++ Smart_living_room_lights/nobody_living_room.lua
@@ -1,19 +1,19 @@
 function CheckHumanInLivingRoomStatus()
     if HumanInLivingRoom == 1 or os.difftime(os.time(), LastTripInLivingRoom) < LivingRoomTimeout then
-            -- log("Movement in kitchen room before delay LivingRoomTimeout seconds, do not turn off light")
+            -- log("Movement in living room before delay LivingRoomTimeout seconds, do not turn off light")
         return true
     end
 
-    -- log("No movement in kitchen room after delay LivingRoomTimeout seconds, turn off light")
+    -- log("No movement in living room after delay LivingRoomTimeout seconds, turn off light")
     turnOffLight(LivingRoomLight)
 end
 
 
 LastTripInLivingRoom = os.time()
--- log("Detected no movement in kitchen room, check all sensors")
+-- log("Detected no movement in living room, check all sensors")
 
 
--- log("All sensors are not tripped. Turn off HumanInLivingRoom indicator and call delay LivingRoomTimeout seconds to turn off kitchen light")
+-- log("All sensors are not tripped. Turn off HumanInLivingRoom indicator and call delay LivingRoomTimeout seconds to turn off living light")
 if isNotTrip(LivingRoomMotionSensor) 
 -- log("Checking to make sure XBMC is stopped")
     and isStopped(XBMC)
